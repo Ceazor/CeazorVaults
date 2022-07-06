bbb7e374dcc4750221ae6a3a24b64e3fe5250d73
Ceazor <ceahorse@gmail.com>
2022-07-06 17:18:11 +0800

    some reaper trys

--- contracts/vaults/CeazorVR.sol
+++ contracts/vaults/CeazorVR.sol
@@ -1,18 +1,14 @@
-/**
- *Submitted for verification at FtmScan.com on 2021-05-10
-*/
-
 // SPDX-License-Identifier: MIT
-// File: @openzeppelin/contracts/utils/Context.sol
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-
+import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 import "../../interfaces/IStrategy.sol";
-
 
 
 pragma solidity ^0.8.11;
@@ -22,7 +18,7 @@ pragma solidity ^0.8.11;
  * This is the contract that receives funds and that users interface with.
  * The yield optimizing strategy itself is implemented in a separate 'Strategy.sol' contract.
  */
-contract CeazorVault is ERC20, Ownable, ReentrancyGuard {
+contract CeazorVaultR is ERC20, Ownable, ReentrancyGuard {
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
 
@@ -34,56 +30,83 @@ contract CeazorVault is ERC20, Ownable, ReentrancyGuard {
     // The last proposed strategy to switch to.
     StratCandidate public stratCandidate;
     // The strategy currently in use by the vault.
-    IStrategy public strategy;
+    address public strategy;
+
+    uint256 public depositFee;
+    uint256 public constant PERCENT_DIVISOR = 10000;
+
+    /**
+    * @dev The strategy's initialization status. Gives deployer 60 minutes after contract
+    * construction (constructionTime) to set the strategy implementation.
+    */
+    bool public initialized = false;
+    uint public constructionTime;
+
+    // The token the vault accepts and looks to maximize.
+    IERC20 public want;
     // The minimum time it has to pass before a strat candidate can be approved.
     uint256 public approvalDelay;
-    // Default initialization of strategy is false, so the intialize function can run once.
-    bool public initialized = false;
 
+    /**
+     * + WEBSITE DISCLAIMER +
+     * Im not a dev, don't put your tokens in here!.
+     */
+
+    /**
+     * @dev simple mappings used to determine PnL denominated in LP tokens,
+     * as well as keep a generalized history of a user's protocol usage.
+     */
+    mapping (address => uint) public cumulativeDeposits;
+    mapping (address => uint) public cumulativeWithdrawals;
 
     event NewStratCandidate(address implementation);
     event UpgradeStrat(address implementation);
+    event TermsAccepted(address user);
+
+    event DepositsIncremented(address user, uint amount, uint total);
+    event WithdrawalsIncremented(address user, uint amount, uint total);
 
     /**
      * @dev Sets the value of {token} to the token that the vault will
-     * hold as underlying value. It initializes the vault's own 'ceaz' token.
+     * hold as underlying value. It initializes the vault's own 'ceazor' token.
      * This token is minted when someone does a deposit. It is burned in order
      * to withdraw the corresponding portion of the underlying assets.
+     * @param _want the token to maximize.
      * @param _name the name of the vault token.
      * @param _symbol the symbol of the vault token.
      * @param _approvalDelay the delay before a new strat can be approved.
-     * @param _want the token to go in the vault.
      */
-    constructor ( 
+    constructor (
         address _want,
         string memory _name,
         string memory _symbol,
-        uint256 _approvalDelay
-    )  ERC20(
-        _name,
-        _symbol
+        uint256 _approvalDelay,
+        uint256 _depositFee
+    ) public ERC20(
+        string(_name),
+        string(_symbol)
     ) {
         want = IERC20(_want);
         approvalDelay = _approvalDelay;
+        constructionTime = block.timestamp;
+        depositFee = _depositFee;
+       
     }
 
+    /**
+     * @dev Connects the vault to its initial strategy. One use only.
+     * @notice deployer has only 60 minutes after construction to connect the initial strategy.
+     * @param _strategy the vault's initial strategy
+     */
 
-
-    // function want() public view returns (IERC20) {
-    //     return IERC20(strategy.want());
-    // }
-
-
-
-        /**
-      * @dev Sets the first strategy that will be used for the vault 
-      */
     function initialize(address _strategy) public onlyOwner returns (bool) {
         require(!initialized, "Contract is already initialized.");
+        require(block.timestamp <= (constructionTime + 3600), "initialization period over, use timelock");
         strategy = _strategy;
         initialized = true;
         return true;
     }
+
 
     /**
      * @dev It calculates the total underlying value of {token} held by the system.
@@ -91,7 +114,7 @@ contract CeazorVault is ERC20, Ownable, ReentrancyGuard {
      *  and the balance deployed in other contracts as part of the strategy.
      */
     function balance() public view returns (uint) {
-        return want.balanceOf(address(this)).add(strategy).balanceOf();
+        return want.balanceOf(address(this)).add(IStrategy(strategy).balanceOf());
     }
 
     /**
@@ -122,23 +145,28 @@ contract CeazorVault is ERC20, Ownable, ReentrancyGuard {
     /**
      * @dev The entrypoint of funds into the system. People deposit with this function
      * into the vault. The vault is then in charge of sending funds into the strategy.
+     * @notice the _before and _after variables are used to account properly for
+     * 'burn-on-transaction' tokens.
+     * @notice to ensure 'owner' can't sneak an implementation past the timelock,
+     * it's set to true
      */
     function deposit(uint _amount) public nonReentrant {
-        require(_amount >0, "U'v put no tokens ser");
-        strategy.beforeDeposit();  
-
+        require(_amount > 0, "please provide amount");
         uint256 _pool = balance();
+        uint256 _before = want.balanceOf(address(this));
         want.safeTransferFrom(msg.sender, address(this), _amount);
-        earn();
-        uint256 _after = balance();
-        _amount = _after.sub(_pool); // Additional check for deflationary tokens
+        uint256 _after = want.balanceOf(address(this));
+        _amount = _after.sub(_before);
+        uint256 _amountAfterDeposit = (_amount.mul(PERCENT_DIVISOR.sub(depositFee))).div(PERCENT_DIVISOR);
         uint256 shares = 0;
         if (totalSupply() == 0) {
-            shares = _amount;
-        } else {
-            shares = (_amount.mul(totalSupply())).div(_pool);
+            shares = _amountAfterDeposit;
+        }else {
+            shares = (_amountAfterDeposit.mul(totalSupply())).div(_pool);
         }
         _mint(msg.sender, shares);
+        earn();
+        incrementDeposits(_amount);
     }
 
     /**
@@ -147,8 +175,8 @@ contract CeazorVault is ERC20, Ownable, ReentrancyGuard {
      */
     function earn() public {
         uint _bal = available();
-        want.safeTransfer(address(strategy), _bal);
-        strategy.deposit();
+        want.safeTransfer(strategy, _bal);
+        IStrategy(strategy).deposit();
     }
 
     /**
@@ -163,52 +191,54 @@ contract CeazorVault is ERC20, Ownable, ReentrancyGuard {
      * from the strategy and pay up the token holder. A proportional number of IOU
      * tokens are burned in the process.
      */
-    function withdraw(uint256 _shares) public {
-        require(_shares > 0, "U'v askt fer no tokens ser");
+    function withdraw(uint256 _shares) public nonReentrant {
+      require(_shares > 0, "please provide amount");
         uint256 r = (balance().mul(_shares)).div(totalSupply());
         _burn(msg.sender, _shares);
 
         uint b = want.balanceOf(address(this));
         if (b < r) {
             uint _withdraw = r.sub(b);
-            strategy.withdraw(_withdraw); //<---- why does this look in IStrategy, where does it get "strategy")
+            IStrategy(strategy).withdraw(_withdraw);
             uint _after = want.balanceOf(address(this));
             uint _diff = _after.sub(b);
             if (_diff < _withdraw) {
                 r = b.add(_diff);
             }
         }
-
         want.safeTransfer(msg.sender, r);
+        incrementWithdrawals(r);
     }
 
-
-    /** 
+    /**
      * @dev Sets the candidate for the new strat to use with this vault.
-     * @param _implementation The address of the candidate strategy.  
+     * @param _implementation The address of the candidate strategy.
      */
     function proposeStrat(address _implementation) public onlyOwner {
         stratCandidate = StratCandidate({
             implementation: _implementation,
             proposedTime: block.timestamp
          });
-
         emit NewStratCandidate(_implementation);
     }
 
-    /** 
-     * @dev It switches the active strat for the strat candidate. After upgrading, the 
-     * candidate implementation is set to the 0x00 address, and proposedTime to a time 
-     * happening in +100 years for safety. 
+    function updateDepositFee(uint256 fee) public onlyOwner {
+        depositFee = fee;
+    }
+
+    /**
+     * @dev It switches the active strat for the strat candidate. After upgrading, the
+     * candidate implementation is set to the 0x00 address, and proposedTime to a time
+     * happening in +100 years for safety.
      */
 
     function upgradeStrat() public onlyOwner {
-        require(stratCandidate.implementation != address(0), "There be no candidate, ser");
-        require(stratCandidate.proposedTime.add(approvalDelay) < block.timestamp, "Wait longa ser");
+        require(stratCandidate.implementation != address(0), "There is no candidate");
+        require(stratCandidate.proposedTime.add(approvalDelay) < block.timestamp, "Delay has not passed");
 
         emit UpgradeStrat(stratCandidate.implementation);
 
-        strategy.retireStrat();
+        IStrategy(strategy).retireStrat();
         strategy = stratCandidate.implementation;
         stratCandidate.implementation = address(0);
         stratCandidate.proposedTime = 5000000000;
@@ -216,14 +246,39 @@ contract CeazorVault is ERC20, Ownable, ReentrancyGuard {
         earn();
     }
 
+    /*
+    * @dev functions to increase user's cumulative deposits and withdrawals
+    * @param _amount number of LP tokens being deposited/withdrawn
+    */
+
+    function incrementDeposits(uint _amount) internal returns (bool) {
+      uint initial = cumulativeDeposits[tx.origin];
+      uint newTotal = initial + _amount;
+      cumulativeDeposits[tx.origin] = newTotal;
+      emit DepositsIncremented(tx.origin, _amount, newTotal);
+      return true;
+    }
+
+
+    function incrementWithdrawals(uint _amount) internal returns (bool) {
+      uint initial = cumulativeWithdrawals[tx.origin];
+      uint newTotal = initial + _amount;
+      cumulativeWithdrawals[tx.origin] = newTotal;
+      emit WithdrawalsIncremented(tx.origin, _amount, newTotal);
+      return true;
+    }
+
+
+    
+
     /**
      * @dev Rescues random funds stuck that the strat can't handle.
-     * @param _token address of the token to rescue.
+     * @param _want address of the token to rescue.
      */
-    function inCaseTokensGetStuck(address _token) external onlyOwner {
-        require(_token != address(want), "!token");
+    function inCaseTokensGetStuck(address _want) external onlyOwner {
+        require(_want != address(want), "!want");
 
-        uint256 amount = IERC20(_token).balanceOf(address(this));
-        IERC20(_token).safeTransfer(msg.sender, amount);
+        uint256 amount = IERC20(_want).balanceOf(address(this));
+        IERC20(_want).safeTransfer(msg.sender, amount);
     }
 }
